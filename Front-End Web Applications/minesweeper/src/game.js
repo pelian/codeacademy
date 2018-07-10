07d314efbc72b95db0312bfeace409b29fb672ca
pelian <joseph@pelian.com>
2018-07-10 16:05:48 +0800

    Transpilation rerun game.js and board.js

--- Front-End Web Applications/minesweeper/src/game.js
+++ Front-End Web Applications/minesweeper/src/game.js
@@ -1,4 +1,14 @@
-import Board from './board.js'
+// To play Minesweeper, we will create instances of MineSweeperGame in command line.
+// For example:
+// In the command line, navigate to the lib directory and run `node`
+// Run `.load game.js` to load the contents of this file.
+// Then create a Game instance and run commands like so:
+// let game = new Game(3, 3, 3);
+// game.playMove(0, 1);
+// game.playMove(1, 2);
+// When done run `.exit`
+
+import { Board } from './board.js'
 
 class Game {
     constructor (numberOfRows, numberOfColumns, numberOfBombs) {
